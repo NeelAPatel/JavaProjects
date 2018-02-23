d87758cfb6fa5db5708bc3ca61930e8f3d173968
Neel <send2neelpatel@gmail.com>
2018-02-23 12:18:42 -0500

    CHESS

---
+++ ChessNeel/src/view/Controller.java
@@ -0,0 +1,23 @@
+package view;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+public class Controller {
+	public void start(Stage mainStage) {
+
+		//MainStage parameters
+		mainStage.setTitle("Song Library");
+
+		
+	}
+}
