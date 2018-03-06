2d2cce7609e0f07cc220c61250ed5ed0fc574328
Neel <send2neelpatel@gmail.com>
2018-03-06 11:59:04 -0500

    dfad

--- testing/src/application/Controller.java
+++ testing/src/application/Controller.java
@@ -10,9 +10,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -24,38 +26,12 @@ public class Controller {
 	@FXML Label lblSelectedName;
 	@FXML FlowPane flow;
 	@FXML ScrollPane scrPane;
+	
+	@FXML TilePane tilePane;
 	int NodeCounter = 0;
 	
 	public void start(Stage mainStage) {
-		
 		mainStage.setTitle("Testing program :DD");
-		scrPane = new ScrollPane();
-		flow = new FlowPane();
-		flow.setPadding(new Insets(5,0,5,0));
-		flow.setVgap(4);
-		flow.setHgap(4);
-		flow.setPrefWrapLength(170);
-	    for (int x = 0; x < 20; x++) {
-	    	  HBox hb = new HBox();
-	            // set hb attributes
-
-	            Button b = new Button("Button" + x);
-	            // set b attributes
-
-	            // then add action listener
-	            b.setOnAction(e->{ 
-	                flow.getChildren().remove(hb); // remove by Object reference
-	            });
-
-	            Label l = new Label("Label" + x);
-	            // set l attributes
-
-	            hb.getChildren().addAll(b,l);
-	            components.add(hb);
-	    }
-	    
-	    flow.getChildren().addAll(components);
-	    
 	}
 	
 	
@@ -76,26 +52,19 @@ public class Controller {
 	
 	
 	public void addProcess() {
-		//FXMLLoader itemLoader = new FXMLLoader();
-		//itemLoader.setLocation(getClass().getResource("Ph_AlbumThumb1.fxml"));
-
+		
 		AnchorPane anch = new AnchorPane();
-		Label lblTest = new Label();
-		lblTest.setText("HELLO, WORLD");
-		lblTest.setPrefWidth(150);
-		lblTest.setPrefHeight(27);
+		ImageView img = new ImageView();
+		Label albumName = new Label();
 		
-		anch.getChildren().add(lblTest);
-		
-		
-		
-		components.add(anch);
 		flow.getChildren().add(new Text("Hello."));
+		
+		
+		
+		
+		
 		System.out.println("Added.");
-		
-		
-		
-		
+				
 	}
 	
 	
